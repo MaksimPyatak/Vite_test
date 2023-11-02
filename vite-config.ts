92102f8e65ab3131526e00c389ec490da3cc926b
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-11-02 10:15:13 +0200

    Added new pages

--- vite-config.ts
+++ vite-config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   // ... other Vite configuration options
@@ -7,6 +8,12 @@ export default defineConfig({
     // ... other build options
     rollupOptions: {
       external: ['backbone'], // Prevent Backbone from being bundled
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'nested/Auth.html'),
+        match: resolve(__dirname, 'nested/match.html'),
+      },
+
     },
   },
 });
